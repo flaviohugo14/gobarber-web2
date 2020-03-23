90176c2b897c5e11a062005cebb113b8aed8a0fb
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-03-23 18:07:07 -0300

    feature: Build Header of dashboard

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoPurple from '~/assets/logo-purple.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logoPurple} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Diego Fernandes</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Flávio"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
