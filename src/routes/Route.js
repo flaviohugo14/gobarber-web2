6e542ed4a6333094693e77dd0638a86505b6ec0e
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-03-22 23:50:40 -0300

    feature: SignIn and SignUp stylized

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -22,7 +22,6 @@ export default function RouteWrapper({
 
   const Layout = signed ? DefaultLayout : AuthLayout;
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
   return (
     <Route
       {...rest}
