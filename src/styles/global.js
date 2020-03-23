70f71a2000c9e3552f1ffb517812290f16903294
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-03-22 23:18:32 -0300

    feature: create global style

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
   * {
     margin: 0;
     padding: 0;
@@ -8,8 +10,12 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  *:focus {
+    outline: 0;
+  }
+
   html, body, #root {
-    min-height: 100%;
+    height: 100%;
   }
 
   body {
@@ -18,9 +24,15 @@ export default createGlobalStyle`
   }
 
   body, input, button {
-    color: #222;
-    font-size: 14px;
-    font-family: Arial, Helvetica, sans-serif;
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
   }
 
   button {
